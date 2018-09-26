e7712474b0ead12449cb4a2c76bedd06e72fb917
Michael Sanders <MichaelSand155@gmail.com>
2018-09-26 18:12:36 +1000

    Initial Push

---
+++ favouriteNumber.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    int favouriteNumber;
+    std::cout << "What's your favourite number? ";
+    std::cin >> favouriteNumber;
+    std::cout << "Wow! " << favouriteNumber << " is my favourite number too!" << std::endl;
+}
